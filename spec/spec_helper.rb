d3ba9f818c1e6ac7d79bc8d104ebbe76cb630013
Dave Pirotte <dpirotte@gmail.com>
2017-07-26 23:04:43 -0500

    Optionally manage etcd when running specs (i.e. for travis)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,5 +4,32 @@ require 'etcdq'
 require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/spec'
+require 'net/http'
+require 'timeout'
 
 Minitest::Reporters.use!
+
+def etcd_running?
+  Net::HTTP.get(URI("http://localhost:2379/health")) == '{"health": "true"}'
+end
+
+def wait_for_etcd(timeout = 2)
+  Timeout.timeout(timeout) do
+    while true do
+      return if etcd_running? rescue nil
+    end
+  end
+end
+
+def start_etcd
+  system("docker-compose up --force-recreate --remove-orphans -d etcd")
+end
+
+def stop_etcd
+  system("docker-compose rm -f -s -v etcd")
+end
+
+if ENV["MANAGE_ETCD"]
+  start_etcd
+  wait_for_etcd
+end
